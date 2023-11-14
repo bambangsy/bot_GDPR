ae252b11247d87995201ec491a7861e185cc0c14
bambangsy <bambangsy28@gmail.com>
2023-11-15 03:59:26 +0700

    add file

---
+++ sl.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import time
+from ml import rqa
+
+
+st.title(f"CHATBOT :blue[GDPR] 🤖")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.subheader(f"Pertanyaan: ")
+    st.code(prompt)
+    st.subheader(f"Respon: ")
+    message_placeholder = st.empty()
+    
+    with st.spinner('Loading...'):
+        result = rqa("PAKAI BAHASA INDONESIA, " + prompt)['result']
+    full_response = ""
+  
+    for chunk in result.split():
+        full_response += chunk + " "
+        time.sleep(0.05)
+        # Add a blinking cursor to simulate typing
+        message_placeholder.markdown(full_response + "▌")
+
+    message_placeholder.markdown(full_response)
+
