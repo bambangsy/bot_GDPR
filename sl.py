d9f33a735d4352663dcec1c269417f2f157dc6fb
bambangsy <bambangsy28@gmail.com>
2024-08-06 15:02:37 +0800

    add

--- sl.py
+++ sl.py
@@ -1,25 +1,30 @@
+from openai import OpenAI
 import streamlit as st
-import time
 from ml import rqa
 
+st.title("ChatGPT-like clone")
 
-st.title(f"TANYA :blue[GDPR] 🤖")
-prompt = st.chat_input("Say something")
-if prompt:
-    st.subheader(f"Pertanyaan: ")
-    st.code(prompt)
-    st.subheader(f"Respon: ")
-    message_placeholder = st.empty()
-    
-    with st.spinner('Loading...'):
+client = OpenAI(api_key=API)
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
         result = rqa("PAKAI BAHASA INDONESIA, " + prompt)['result']
-    full_response = ""
-  
-    for chunk in result.split():
-        full_response += chunk + " "
-        time.sleep(0.05)
-        # Add a blinking cursor to simulate typing
-        message_placeholder.markdown(full_response + "▌")
-
-    message_placeholder.markdown(full_response)
-
+        full_response = ""
+        for chunk in result.split():
+            full_response += chunk + " "
+        st.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
